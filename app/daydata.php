910a63c5ef3325abd4d65ee20963ccc01a233d09
岩﨑　健一 <e1659106@gmail.com>
2019-06-13 10:34:16 +0800

    This is input

--- app/daydata.php
+++ app/daydata.php
@@ -3,6 +3,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 class Daydata extends Model
 {   protected $table = 'daydata';
+    protected $guarded = ['id'];
     public function earthquakes()
     {
         return $this->belongsTo('App\Earthquakes','year','Month','Day','value');
@@ -11,4 +12,5 @@ class Daydata extends Model
     {
         return $this->belongsTo('App\answer','year','Month','Day','value');
     }
+
 }
