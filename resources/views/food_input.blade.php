9cca737ba1aeb65515a5ac52088f75aaa3688e4a
岩﨑　健一 <e1659106@gmail.com>
2019-06-14 12:27:23 +0800

    This is foodlist

---
+++ resources/views/food_input.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-8">
+        <form method="post" action="{{ route('fpost') }}">
+        @csrf
+        <div class="form-group row">
+            <label for="foodname" class="col-md-4 col-form-label text-md-right">food name</label>
+                <div class="col-md-6">
+                    <input type="text" class="form-control {{ $errors->has('foodname') ? ' is-invalid' : '' }}"
+                     name="foodname" value="foodname">
+                    @if ($errors->has('foodname'))
+                    <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('foodname') }}</strong>
+                    </span>
+                    @endif
+                </div>
+        </div>
+        <div class="form-group row">
+        <label for="date" class="col-md-4 col-form-label text-md-right">Consumption period</label>
+            <div class="col-md-6">
+            <input type="date" class="form-control {{ $errors->has('date') ? ' is-invalid' : '' }}"
+            name="date" value="yyyy/mm/dd">
+            @if ($errors->has('date'))
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('date') }}</strong>
+            </span>
+            @endif
+        <a href="/food/inputdata"><button>Input</button></a>
+        </form>
+        <a href="/food"><button>back</button></a>
+        </div>
+    </div>
+</div>
+@endsection
+                    
--- resources/views/foodlist.blade.php
+++ resources/views/foodlist.blade.php
@@ -1,4 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>This is food list page</h1>
+<div class="row">
+    <div class="col-sm-9"></div>
+    <div class="col-sm-3">
+        <a href="/food/input"><button>input</button></a>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-3"></div>
+    <div class="col-sm-6">
+    <table class="table table-striped table-bordered">
+        <tr>
+            <th>food</th>
+            <th>Consumption date</th>
+            <th></th>
+        </tr>
+        @foreach($foods as $food)
+        <tr>
+            <td>{{ $food->foodname }}</td>
+            <td>{{ $food->date }}</td>
+            <td><button><a href="food/edit/{{ $food->id }}">edit</a></button>
+            <button><a href="food/delete/{{ $food->id }}">delete</a></button></td>
+            </tr>
+        @endforeach
+    </table>
+    </div>
+    <div class="col-sm-3">
+    </div>
+</div>
+@endsection
