cf293418711710fb01631da2fd86880807793395
岩﨑　健一 <e1659106@gmail.com>
2019-06-18 12:38:38 +0800

    This is input

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -8,6 +8,7 @@
       @csrf
       <label>Year</label>
         <select name="year row=6">
+          <option value="2019">2019</option>
           <option value="2020">2020</option>
           <option value="2021">2021</option>
           <option value="2022">2022</option>
